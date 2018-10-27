d09d2a60b0852d177d6e1b6842fab0dfd9e4bc18
Michael <mkaplor@gmail.com>
2018-10-27 00:58:41 -0500

    restart push

--- Assets/P_Scipts/TestCharacterController.cs
+++ Assets/P_Scipts/TestCharacterController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class TestCharacterController : MonoBehaviour {
 
+
+    [SerializeField]
+    float moveSpeed;
+
+    Vector3 forward, right;
+
 	// Use this for initialization
 	void Start () {
-		
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Push();
 	}
+
+    void Push()
+    {
+        
+    }
 }
