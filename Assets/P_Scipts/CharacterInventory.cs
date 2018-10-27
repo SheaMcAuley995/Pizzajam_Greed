d162145d668590a8b046de43071541a5334e5bde
Unknown <s179049@students.aie.edu.au>
2018-10-26 22:12:29 -0700

    I need this

--- Assets/P_Scipts/CharacterInventory.cs
+++ Assets/P_Scipts/CharacterInventory.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour {
+public class CharacterInventory : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
