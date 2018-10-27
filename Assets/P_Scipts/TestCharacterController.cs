91577138ac503ff86ba5f9a28ccd86b9d7174e33
Michael <mkaplor@gmail.com>
2018-10-27 11:06:28 -0500

    for being gone for a bit

--- Assets/P_Scipts/TestCharacterController.cs
+++ Assets/P_Scipts/TestCharacterController.cs
@@ -7,16 +7,16 @@ public class TestCharacterController : MonoBehaviour {
 
     [SerializeField]
     float moveSpeed = 2;
-
+    Rigidbody rb;
     Vector3 forward, right;
 
 	// Use this for initialization
 	void Start () 
     {
-        
+        rb = GetComponent<Rigidbody>();   
         forward = Camera.main.transform.forward;
         forward.y = 0;
-        forward = Vector3.Normalize(forward);
+        //forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 
 	}
@@ -32,20 +32,21 @@ public class TestCharacterController : MonoBehaviour {
 
     void Push()
     {
-       Vector3 dir = new Vector3(Input.GetAxis("h_move"), 0, Input.GetAxis("v_move"));
+       //Vector3 dir = new Vector3(Input.GetAxis("h_move"), 0, Input.GetAxis("v_move"));
 
 
         Vector3 rightMove = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         Vector3 upMove = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-
-        Vector3 heading = Vector3.Normalize(rightMove + upMove);
+        Debug.Log(upMove + "upmove");
+        Vector3 heading = Vector3.Normalize(rightMove + upMove) * moveSpeed;
 
         //determines rotation
         transform.forward = heading;
 
         //make movement
+        rb.AddForce(upMove);
         transform.position += heading;
-        transform.position += upMove;
+        //transform.position += upMove;
 
 
     }
