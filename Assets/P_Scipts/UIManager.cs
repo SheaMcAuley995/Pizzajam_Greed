f54d96c2a0734d93b623428e994c9cc87b43cbd5
Michael <mkaplor@gmail.com>
2018-10-27 22:51:50 -0800

    git that shit

---
+++ Assets/P_Scipts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField]
+    public Scene gameScene;
+
+    public void pressedPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void pressedGTFO()
+    {
+        Application.Quit();
+    }
+
+}
