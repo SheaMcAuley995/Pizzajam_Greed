c25ac818c8bfca4273920a3cdc381d8d8b7b3675
Unknown <s179049@students.aie.edu.au>
2018-10-27 23:53:21 -0700

    It is my stuffffff

--- Assets/P_Scipts/InventoryUI.cs
+++ Assets/P_Scipts/InventoryUI.cs
@@ -1,17 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
 public class InventoryUI : MonoBehaviour {
 
-    
+    [SerializeField] TextMeshProUGUI numSlot_One;
+    [SerializeField] TextMeshProUGUI numSlot_Two;
+    [SerializeField] TextMeshProUGUI numSlot_Three;
+    [SerializeField] TextMeshProUGUI MultiplierNum;
 
     [SerializeField] CharacterInventory inventory;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
-	}
+
+        unoptimizedGarbageUpdateGUI();
+
+    }
+    
+    private void unoptimizedGarbageUpdateGUI()
+    {
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+
+        
+        MultiplierNum.text = inventory.scoreModifier.ToString();
+        foreach(ItemObjects obj in inventory.items)
+        {
+            if(obj.name == "Ball")
+            {
+                num1++;
+            }
+            else if(obj.name == "Coin")
+            {
+                num2++;
+            }
+            else if(obj.name == "Diamond")
+            {
+                num3++;
+            }
+        }
+        numSlot_One.text = num1.ToString();
+        numSlot_Two.text = num2.ToString();
+        numSlot_Three.text = num3.ToString();
+    }
+
+    
+   
 }
